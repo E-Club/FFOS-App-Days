b6c91c2e54e5d11aba58cf6d6a364a954d76340b
Sahil Singh <sahilsingh10394@gmail.com>
2014-09-28 03:48:54 +0530

    Backend Changes
    Basic fn is working now

--- backend2.js
+++ backend2.js
@@ -6,22 +6,23 @@ for(var i =0; i <keys.length; i++)
 {
 	keys[i].onclick = function(e)
 	{
-		input.innerHTML = ' ';
 		var input = document.querySelector('.screen');
 		var inputVal = input.innerHTML;
 		var btnVal = this.innerHTML;
 		
+		
 		if(btnVal == 'C')
 		{
-			input.innerHTML = ' ';
-			decimalAdded = false;
+			input.innerHTML = '';
 		}
 		else if(btnVal == '=')
 		{
 			var equation = inputVal;
-			var lastChar = equation[equation.length - 1];
-			
 			input.innerHTML = eval(equation);
+		}
+		else
+		{
+			input.innerHTML += btnVal;
 		}
 	}
 }	
