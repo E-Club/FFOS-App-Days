6fd4b788e5b9c247b9602c655593316d6de14de4
Sahil Singh <sahilsingh10394@gmail.com>
2014-09-28 03:15:31 +0530

    Work on backend2
    First Test code on backend2

--- backend2.js
+++ backend2.js
@@ -1,1 +1,28 @@
 var keys = document.querySelectorAll('#calculator span');
+var operators = ['+','-','*','/'];
+
+
+for(var i =0; i <keys.length; i++)
+{
+	keys[i].onclick = function(e)
+	{
+		input.innerHTML = ' ';
+		var input = document.querySelector('.screen');
+		var inputVal = input.innerHTML;
+		var btnVal = this.innerHTML;
+		
+		if(btnVal == 'C')
+		{
+			input.innerHTML = ' ';
+			decimalAdded = false;
+		}
+		else if(btnVal == '=')
+		{
+			var equation = inputVal;
+			var lastChar = equation[equation.length - 1];
+			
+			input.innerHTML = eval(equation);
+		}
+	}
+}	
+	
